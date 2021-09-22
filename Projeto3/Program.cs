5aa000d2cca2cf589fc807bed6c7d7dc6debc163
Jean Mensen <jeanmensen@gmail.com>
2021-09-22 19:58:58 -0300

    Projeto iniciado

---
+++ Projeto3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+
+namespace Projeto3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaBancaria conta;
+
+            Console.Write("Entre o número da Conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da Conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char resp = char.Parse(Console.ReadLine());
+            if (resp == 's' || resp == 'S')
+            {
+                Console.Write("Entre o valor de Depósto Inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(numero, titular, depositoInicial);
+            }
+            else
+            {
+                conta = new ContaBancaria(numero, titular);
+            }
+
+            Console.WriteLine(conta);
+
+            Console.WriteLine("Entre um valor para depósito: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);
+
+            Console.WriteLine(conta);
+
+
+            Console.WriteLine("Entre um valor para saque: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(quantia);
+
+            Console.Write(conta);
+
+
+
+
+
+        }
+
+
+
+
+
+    }
+}
+
